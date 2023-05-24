21a10222a5a30aec37aca6e62b76f5326622ee82
GhislainFievet <ghislain.fievet@gmail.com>
2023-05-24 16:54:21 +0200

    remove setClass

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,9 +1,5 @@
 # Define show methods for large vectors and data.frames
 .onLoad <- function(libname, pkgname){
-    setClass("advChar",
-         slots = list(
-           values = "character"
-         ))
     setMethod("show", "advChar",
           function(x) {
               if ( length(x@values)<10){
@@ -18,10 +14,6 @@
                   }
               }
           })
-    setClass("advList",
-         slots = list(
-           values = "list"
-         ))
     setMethod("show", "advList",
           function(x) {
               if ( length(x@values)<10){
