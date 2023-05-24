43873718bd7ad5bb94bc8f895252dd8cd156bf0b
GhislainFievet <ghislain.fievet@gmail.com>
2023-05-24 19:30:26 +0200

    remove onLoad

--- R/advMaxChange.R
+++ R/advMaxChange.R
@@ -135,6 +135,24 @@ advMaxChange <- function(exprs, clusters, target, classifier,
     if ( is.null(lResults)){
         NULL
     } else {
+        methods::setClass("advChar",
+         slots = list(
+           values = "character"
+         ))
+        setMethod("show", "advChar",
+          function(x) {
+              if ( length(x@values)<10){
+                  print(x@values)
+              } else {
+                  print(paste0("Vector with ", length(x@values), " values:"))
+                  if (is.null(names(x@values))){
+                      print(paste0(paste(x@values[1:10], collapse=", "), " ..."))
+                  } else {
+                      print(paste0(paste(paste0(names(x@values[1:5]),": ",x@values[1:5]),
+                                         collapse=", "), " ..."))
+                  }
+              }
+          })
         new("advChar", values=lResults)
     }
 }
